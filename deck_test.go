ad048e3eae4b6c11d9fd2bfa8f1df9e66a3acbe8
Kevin Welch <kaedub16@gmail.com>
2022-04-21 01:37:12 -0700

    Added a mutative removeMany method to deck that remove cards at given indices

--- deck_test.go
+++ deck_test.go
@@ -26,7 +26,7 @@ func TestDeck(t *testing.T) {
 	deck.shuffle()
 	e, f, g, h := deck[0], deck[1], deck[2], deck[3]
 	if a == e && b == f && c == g && d == h {
-		t.Errorf("Shuffle did not shuffle cards")
+		t.Errorf("deck.shuffle did not shuffle cards")
 	}
 
 	// assert that draw removes a card from the top of the deck
@@ -44,10 +44,24 @@ func TestDeck(t *testing.T) {
 	selectedCard := deck[5]
 	removedCard := deck.remove(5)
 	if selectedCard != removedCard {
-		t.Errorf("deck.remove did not get the correct card: %v vs %v", selectedCard, removedCard)
+		t.Errorf("deck.remove did not get the correct card:\nexpected -> %v\nreceived-> %v", selectedCard, removedCard)
 	}
 	if len(deck) != 51 {
-		t.Errorf("deck.remove did not remove the card: %v vs %v", selectedCard, removedCard)
+		t.Errorf("deck.remove did not remove the card:\nexpected -> %v\nreceived -> %v", selectedCard, removedCard)
+	}
+
+	// assert that multiple cards can be removed from the deck
+	deck = newDeck()
+	selectedCards := []Card{deck[1], deck[4], deck[20]}
+	removedCards := deck.removeMany([]int{1, 4, 20})
+	expectedDeckLength := 52 - len(selectedCards)
+	if len(deck) != expectedDeckLength {
+		t.Errorf("deck.removeMany did not remove the correct number of cards:\nexpected -> %v\nreceived -> %v", expectedDeckLength, len(deck))
+	}
+	for i, card := range selectedCards {
+		if card.String() != removedCards[i].String() {
+			t.Errorf("deck.removeMany did not remove the correct cards:\nexpected -> %v\nreceived -> %v", card, removedCards[i])
+		}
 	}
 
 	// assert that deck will sort cards by value
